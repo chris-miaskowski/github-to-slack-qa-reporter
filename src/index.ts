82d9d0d4d9f3d360fac023ce31ce8defc94e3c7b
Chris Miaskowski <chris@11sigma.com>
2020-05-25 12:13:33 +0200

    use files to store token and wh

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import * as fs from 'fs';
 import { Octokit } from '@octokit/rest';
+// @ts-ignore
+import * as Slack from 'slack-notify';
 
-const token = fs.readFileSync(`${__dirname}/../.env`, { encoding: 'utf8' });
+const token = fs.readFileSync(`${__dirname}/../.ghtoken`, { encoding: 'utf8' });
+const webhook = fs.readFileSync(`${__dirname}/../.slackwh`, { encoding: 'utf8' });
 
 // https://octokit.github.io/
 const octokit = new Octokit({
@@ -18,9 +21,13 @@ const octokit = new Octokit({
   },
 });
 
+const slack = Slack(webhook);
+
 // https://help.github.com/en/github/searching-for-information-on-github/searching-issues-and-pull-requests
 octokit.search.issuesAndPullRequests({
-  q: 'repo:chris-miaskowski/github-to-slack-qa-reporter+label:bug+type:issue'
+  q: 'repo:chris-miaskowski/github-to-slack-qa-reporter+label:bug+type:issue+created:2020-05-24..2020-05-24'
 }).then(response => {
-  console.log('Bugs reported:', response.data.total_count);
+  // https://www.npmjs.com/package/slack-notify
+  slack.bug(`Bugs reported: ${response.data.total_count}`);
 });
+
