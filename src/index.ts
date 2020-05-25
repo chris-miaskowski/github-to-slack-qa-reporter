c0587b31558c669cf56cb5282ca56f6c87a950a7
Chris Miaskowski <chris@11sigma.com>
2020-05-25 13:00:44 +0200

    add some simple stats

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import * as fs from 'fs';
 import { Octokit } from '@octokit/rest';
-// @ts-ignore
 import * as Slack from 'slack-notify';
+import * as dotenv from 'dotenv';
+import * as moment from 'moment';
 
-const token = fs.readFileSync(`${__dirname}/../.ghtoken`, { encoding: 'utf8' });
-const webhook = fs.readFileSync(`${__dirname}/../.slackwh`, { encoding: 'utf8' });
+dotenv.config();
+
+const { GH_TOKEN, GH_REPO, SLACK_WEBHOOK } = process.env;
 
 // https://octokit.github.io/
 const octokit = new Octokit({
-  auth: token,
-  userAgent: 'gh2slackqareporter v0.0.0',
+  auth: GH_TOKEN,
+  userAgent: 'gh2slackbugreporter v0.0.0',
   previews: ['jean-grey'],
   timeZone: 'Europe/Warsaw',
   baseUrl: 'https://api.github.com',
@@ -21,13 +23,52 @@ const octokit = new Octokit({
   },
 });
 
-const slack = Slack(webhook);
+const slack = Slack(SLACK_WEBHOOK);
 
-// https://help.github.com/en/github/searching-for-information-on-github/searching-issues-and-pull-requests
-octokit.search.issuesAndPullRequests({
-  q: 'repo:chris-miaskowski/github-to-slack-qa-reporter+label:bug+type:issue+created:2020-05-24..2020-05-24'
-}).then(response => {
+async function dailyReport() {
+  const FORMAT = 'YYYY-MM-DD';
+  const yesterday = moment().subtract(1, 'days').format(FORMAT);
+  const ago2Days = moment().subtract(2, 'days').format(FORMAT);
+  const ago7Days = moment().subtract(7, 'days').format(FORMAT);
+  const ago14Days = moment().subtract(14, 'days').format(FORMAT);
+  const ago30days = moment().subtract(30, 'days').format(FORMAT);
+  const ago60days = moment().subtract(60, 'days').format(FORMAT);
+  const today = moment().format(FORMAT);
+
+  // https://help.github.com/en/github/searching-for-information-on-github/searching-issues-and-pull-requests
+  const daily = await octokit.search.issuesAndPullRequests({
+    q: `repo:${GH_REPO}+label:bug+type:issue+created:${yesterday}..${today}`
+  });
+
+  const dailyPrev = await octokit.search.issuesAndPullRequests({
+    q: `repo:${GH_REPO}+label:bug+type:issue+created:${ago2Days}..${yesterday}`
+  });
+
+  const last7Days = await octokit.search.issuesAndPullRequests({
+    q: `repo:${GH_REPO}+label:bug+type:issue+created:${ago7Days}..${today}`
+  });
+
+  const last7DaysPrev = await octokit.search.issuesAndPullRequests({
+    q: `repo:${GH_REPO}+label:bug+type:issue+created:${ago14Days}..${ago7Days}`
+  });
+
+  const last30Day = await octokit.search.issuesAndPullRequests({
+    q: `repo:${GH_REPO}+label:bug+type:issue+created:${ago30days}..${today}`
+  });
+
+  const last30DayPrev = await octokit.search.issuesAndPullRequests({
+    q: `repo:${GH_REPO}+label:bug+type:issue+created:${ago60days}..${ago30days}`
+  });
+
   // https://www.npmjs.com/package/slack-notify
-  slack.bug(`Bugs reported: ${response.data.total_count}`);
-});
+  slack.note({
+    text: `QA Daily Bug Report. Bugs since (vs past period):`,
+    fields: {
+      'Yesterday': `${daily.data.total_count} (dailyPrev.data.total_count})`,
+      '7 days ago': `${last7Days.data.total_count} (${last7DaysPrev.data.total_count})`,
+      '30 days ago': `${last30Day.data.total_count} (${last30DayPrev.data.total_count})`,
+    },
+  });
+}
 
+dailyReport();
