fc639947fd0d6176bdde6d9d5835cfafc9f7cdd6
Chris Miaskowski <chris@11sigma.com>
2020-06-03 15:24:22 +0200

    feat: add more stats

--- scripts/index.ts
+++ scripts/index.ts
@@ -4,9 +4,14 @@ import { dailyReport } from "../src";
 dotenv.config();
 
 const { GH_TOKEN, GH_REPO, SLACK_WEBHOOK } = process.env;
+const bug = 't/bug';
 
 dailyReport({
   ghRepo: GH_REPO!,
   ghToken: GH_TOKEN!,
   slackWebhook: SLACK_WEBHOOK!,
-});
+}, bug, [
+  [bug],
+  [bug, 'cs/reported'],
+  [bug, 'p/urgent'],
+]);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
+import { OctokitResponse, SearchIssuesAndPullRequestsResponseData } from '@octokit/types';
 import { Octokit } from '@octokit/rest';
 import * as moment from 'moment';
 // @ts-ignore
 import * as Slack from 'slack-notify';
 
-export async function dailyReport(options: { ghToken: string; ghRepo: string; slackWebhook: string }) {
+export async function dailyReport(options: { ghToken: string; ghRepo: string; slackWebhook: string }, bugLabel: string, sections: string[][]) {
   // https://octokit.github.io/
   const octokit = new Octokit({
     auth: options.ghToken,
@@ -22,44 +23,65 @@ export async function dailyReport(options: { ghToken: string; ghRepo: string; sl
   const slack = Slack(options.slackWebhook);
 
   const FORMAT = 'YYYY-MM-DD';
-  const yesterday = moment().subtract(1, 'days').format(FORMAT);
-  const ago2Days = moment().subtract(2, 'days').format(FORMAT);
-  const ago7Days = moment().subtract(7, 'days').format(FORMAT);
-  const ago14Days = moment().subtract(14, 'days').format(FORMAT);
-  const ago30days = moment().subtract(30, 'days').format(FORMAT);
-  const ago60days = moment().subtract(60, 'days').format(FORMAT);
   const today = moment().format(FORMAT);
-  const label = 't/bug';
+  const firstDayOfThisMonth = moment().startOf('month').format(FORMAT);
+  const firstDayOfPrevMonth = moment().subtract(1, 'months').startOf('month').format(FORMAT);
+  const lastDayOfPrevMonth = moment().subtract(1, 'months').endOf('month').format(FORMAT);
 
   async function queryGH(from: string, to: string) {
-    const q = `repo:${options.ghRepo}+label:${label}+type:issue+created:${from}..${to}`;
+    const q = `repo:${options.ghRepo}+label:${bugLabel}+type:issue+created:${from}..${to}`;
     console.log('Query', q);
     return octokit.search.issuesAndPullRequests({ q });
   }
 
   // https://help.github.com/en/github/searching-for-information-on-github/searching-issues-and-pull-requests
-  const daily = await queryGH(yesterday, today);
-  const dailyPrev = await queryGH(ago2Days, yesterday);
-  const last7Days = await queryGH(ago7Days, today);
-  const last7DaysPrev = await queryGH(ago14Days, ago7Days);
-  const last30Day = await queryGH(ago30days, today);
-  const last30DayPrev = await queryGH(ago60days, ago30days);
+  const lastMonth = await queryGH(firstDayOfPrevMonth, lastDayOfPrevMonth);
+  const thisMonth = await queryGH(firstDayOfThisMonth, today);
 
-  // https://www.npmjs.com/package/slack-notify
-  const dailyChange = change(dailyPrev, daily);
-  const weeklyChange = change(last7DaysPrev, last7Days);
-  const monthlyChange = change(last30DayPrev, last30Day);
   slack.note({
-    text: `*QA's :bug: Report (${options.ghRepo})*.
-
-Reported bugs in range (+/- previous period):
-- 1 day: ${daily.data.total_count} (${dailyChange})
-- 7 days: ${last7Days.data.total_count} (${weeklyChange})
-- 30 days: ${last30Day.data.total_count} (${monthlyChange})`
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: 'Howdy bug chasers! :wave:'
+        }
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Here is your daily bug report for \`${options.ghRepo}\` repository.`
+        }
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `_We compare this month (${firstDayOfThisMonth} - ${today}) to the entire previous month (${firstDayOfPrevMonth} - ${lastDayOfPrevMonth})._`
+        }
+      },
+      sections.map(section => {
+        return {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: stats(section, thisMonth, lastMonth)
+          }
+        };
+      })
+    ]
   });
 }
 
-function change(a: any, b: any) {
-  const c = b.data.total_count - a.data.total_count;
-  return Math.sign(c) > 0 ? `+${c}` : (Math.sign(c) < 0 ? `-${c}` : c);
+function stats(labels: string[], issuesThisMonth: OctokitResponse<SearchIssuesAndPullRequestsResponseData>, issuesLastMonth: OctokitResponse<SearchIssuesAndPullRequestsResponseData>) {
+  const totalThisMonth = issuesThisMonth.data.items.filter(item => labels.every(label => item.labels.map(l => l.name).includes(label)));
+  const totalLastMonth = issuesLastMonth.data.items.filter(item => labels.every(label => item.labels.map(l => l.name).includes(label)));
+  const closedThisMonth = totalThisMonth.filter(item => item.state === 'closed');
+  const closedLastMonth = totalLastMonth.filter(item => item.state === 'closed');
+
+  return `*\`${labels.join(' ')}\` issues:*
+
+  • total: \`${totalThisMonth.length}\` vs. \`${totalLastMonth.length}\`
+  • closed: \`${closedThisMonth.length}\` vs. \`${closedLastMonth.length}\``
 }
